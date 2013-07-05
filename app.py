172a523fe6f03b240148c31c26ff960ba4989441
sykp <sykp241095@gmail.com>
2013-07-05 14:25:08 +0800

    add more import

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import urllib2
 import logging
 from urllib import urlencode
 from collections import defaultdict
-from bottle import route, run, template, static_file, request, redirect
+from bottle import route, run, template, static_file, request, redirect, default_app
 
 import config
 
