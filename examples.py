7894c449ddd7014e1dc8b578cca0500f2f6054c7
sykp <sykp241095@gmail.com>
2013-09-27 00:45:01 +0800

    rm unused

--- examples.py
+++ examples.py
@@ -1,10 +1,10 @@
 examples = [
     'dba1.mysql.*\.select group by 3',
-    'thorin.beansdb group by 4',
-    'thorin.doubanmemcache.*11211 group by 3',
-    'thorin.doubanmemcache.*11212 group by 3',
-    'thorin.doubanmemcache.*11213 group by 3',
-    'thorin.doubanmemcache.*11214 group by 3',
-    'thorin.beansmq group by 3',
     'thorin.iostat group by 3',
+    'beansdb group by 4',
+    'beansmq group by 3',
+    'doubanmemcache.*11211 group by 3',
+    'doubanmemcache.*11212 group by 3',
+    'doubanmemcache.*11213 group by 3',
+    'doubanmemcache.*11214 group by 3',
 ]
