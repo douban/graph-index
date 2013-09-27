c25813f70b554ac50ed69cd44c3b8f22d22a2843
sykp <sykp241095@gmail.com>
2013-09-27 11:28:52 +0800

    add more example

--- examples.py
+++ examples.py
@@ -1,6 +1,7 @@
 examples = [
     'dba1.mysql.*\.select group by 3',
     'thorin.iostat group by 3',
+    'bandwidth.*(in|out)',
     'beansdb group by 4',
     'beansmq group by 3',
     'doubanmemcache.*11211 group by 3',
