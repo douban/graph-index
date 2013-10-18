e6c3eeec77c39f0c3fdca4ee8a330c6d29377fd0
sykp <sykp241095@gmail.com>
2013-10-18 14:47:17 +0800

    set false

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 graphite_url = 'http://graphite.intra.douban.com'
 graphite_index_url = graphite_url + '/metrics/index.json'
 metrics_file = 'metrics.json'
-debug = True
+debug = False
 listen_host = '0.0.0.0'
 listen_port = 8808
 try:
