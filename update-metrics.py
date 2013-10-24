41c6916f0c78da32a7cc50a5ae1e7a17f68f2ef2
sykp <sykp241095@gmail.com>
2013-10-24 17:02:26 +0800

    drop output

--- update-metrics.py
+++ update-metrics.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 from utils import logging, build_metrics, build_diamond
 if __name__ == '__main__':
-    logging.info('build metrics...')
     metrics = build_metrics()
-    logging.info('build diamond...')
     build_diamond(metrics)
